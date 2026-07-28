529fa4afd0a0ea3d12b26650529241b396a1a881
dmytro tverdokhlib <ditverd@gmail.com>
2026-07-28 22:14:01 +0300

    Subject

---
+++ src/Entity/Subject.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: SubjectRepository::class)]
+class Subject
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private Uuid $id;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
