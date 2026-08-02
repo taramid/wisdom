d856ecdb3913793553fa3635a6c0ed38973e3277
dmytro tverdokhlib <ditverd@gmail.com>
2026-08-02 19:10:40 +0300

    fix

--- src/Security/SimpleAuthenticator.php
+++ src/Security/SimpleAuthenticator.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\User\InMemoryUser;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -35,7 +34,7 @@ class SimpleAuthenticator extends AbstractLoginFormAuthenticator
         $submittedPassword = $request->request->getString('password');
 
         return new Passport(
-            new UserBadge('creator', fn() => new InMemoryUser('creator', null, ['ROLE_CREATOR'])),
+            new UserBadge('creator'),
             new CustomCredentials(
 //                fn(string $password) => hash_equals(trim($this->creatorPassword), trim($password)),
                 fn($password) => hash_equals($this->creatorPassword, (string) $password),
