48c1650e2a6174f980a1e4e5a40d629a7b3cafe8
dmytro tverdokhlib <ditverd@gmail.com>
2026-08-02 18:36:21 +0300

    fix

--- src/Security/SimpleAuthenticator.php
+++ src/Security/SimpleAuthenticator.php
@@ -3,6 +3,7 @@
 namespace App\Security;
 
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
