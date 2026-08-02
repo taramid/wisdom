43863c42ca85b61b240c4323dec7245a541c49cd
dmytro tverdokhlib <ditverd@gmail.com>
2026-08-02 18:30:39 +0300

    from gemini (as all before, lol)

--- src/Security/SimpleAuthenticator.php
+++ src/Security/SimpleAuthenticator.php
@@ -2,34 +2,63 @@
 
 namespace App\Security;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\InMemoryUser;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 /**
  * @see https://symfony.com/doc/current/security/custom_authenticator.html
  */
-class SimpleAuthenticator extends AbstractAuthenticator
+class SimpleAuthenticator extends AbstractLoginFormAuthenticator
 {
+    use TargetPathTrait;
+
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        #[Autowire('%env(APP_CREATOR_PASSWORD)%')] private string $creatorPassword
+    ) {}
+
     /**
      * Called on every request to decide if this authenticator should be
      * used for the request. Returning `false` will cause this authenticator
      * to be skipped.
      */
-    public function supports(Request $request): ?bool
+    public function supports(Request $request): bool
     {
         // return $request->headers->has('X-AUTH-TOKEN');
     }
 
     public function authenticate(Request $request): Passport
     {
+        // У Symfony 8.1 краще використовувати getString() замість (string) $request->request->get()
+        $submittedPassword = $request->request->getString('password');
+
+        return new Passport(
+            new UserBadge('creator', fn() => new InMemoryUser('creator', null, ['ROLE_CREATOR'])),
+            new CustomCredentials(
+                fn($password) => hash_equals($this->creatorPassword, (string) $password),
+                $submittedPassword
+            ),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->getString('_csrf_token')),
+            ]
+        );
+
+
         // $apiToken = $request->headers->get('X-AUTH-TOKEN');
         // if (null === $apiToken) {
         // The token header was empty, authentication fails with HTTP Status
@@ -47,7 +76,13 @@ class SimpleAuthenticator extends AbstractAuthenticator
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         // on success, let the request continue
-        return null;
+
+        // Повертаємо юзера на сторінку, куди він йшов до авторизації
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('app_wisdom_index'));
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
